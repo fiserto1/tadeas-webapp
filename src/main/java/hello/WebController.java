7085ed43f1b6e450a6ca50042ebce891a37ace8e
Matěj Mihal <mejatysek@gmail.com>
2017-12-17 22:04:05 +0100

    added siple login form not connected to backend

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import hello.bussiness.forms.Login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,13 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class WebController {
@@ -28,5 +35,23 @@ public class WebController {
         log.trace("Message translation: {}", greeting);
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("form", new Login());
+        return "login";
+    }
+    @PostMapping("/login")
+    public String loginSend(@Valid @ModelAttribute("form") Login login, final BindingResult bindingResult, Model model) {
+//        model.addAttribute("loginForm", login);
+        if(bindingResult.hasErrors()){
+            log.trace(bindingResult.getFieldError("login").getDefaultMessage());
+//            System.out.println(bindingResult.getFieldError("login").getDefaultMessage());
+//            model.addAttribute("form", login);
+            return "login";}
+        model.addAttribute("login", login.getLogin());
+        model.addAttribute("password", login.getPassword());
+        return "loged";
     }
 }
---
+++ src/main/java/hello/bussiness/forms/Login.java
@@ -0,0 +1,29 @@
+package hello.bussiness.forms;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+
+public class Login {
+        @Email
+        private String login;
+        @NotEmpty
+        private String password;
+
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+}
