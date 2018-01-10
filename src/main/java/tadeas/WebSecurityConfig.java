ac0205068a80d81c07ea4305476916cbeeddd509
Matěj Mihal <mejatysek@gmail.com>
2018-01-10 21:37:53 +0100

    added login form frontend validations

--- src/main/java/tadeas/WebSecurityConfig.java
+++ src/main/java/tadeas/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/webjars/**", "/css/**").permitAll()
+                .antMatchers("/webjars/**", "/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
